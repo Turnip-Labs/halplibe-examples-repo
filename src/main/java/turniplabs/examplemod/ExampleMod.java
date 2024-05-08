61962c6ec35423db6feb70dcbf56ae01bc2ac481
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2024-05-08 03:26:57 -0500

    updated recipebuilder to 7.2

--- src/main/java/turniplabs/examplemod/ExampleMod.java
+++ src/main/java/turniplabs/examplemod/ExampleMod.java
@@ -3,16 +3,12 @@ package turniplabs.examplemod;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.core.WeightedRandomLootObject;
 import net.minecraft.core.block.Block;
-import net.minecraft.core.data.registry.Registries;
 import net.minecraft.core.item.Item;
-import net.minecraft.core.item.ItemDye;
 import net.minecraft.core.item.ItemStack;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import turniplabs.halplibe.helper.BlockBuilder;
 import turniplabs.halplibe.helper.RecipeBuilder;
 import turniplabs.halplibe.helper.recipeBuilders.RecipeBuilderShaped;
-import turniplabs.halplibe.util.GameStartEntrypoint;
 import turniplabs.halplibe.util.RecipeEntrypoint;
 
 
@@ -24,6 +20,13 @@ public class ExampleMod implements ModInitializer, RecipeEntrypoint {
         LOGGER.info("ExampleMod initialized.");
     }
 	@Override
+	public void initNamespaces() {
+		// Initializes each workstation for our modid
+		RecipeBuilder.initNameSpace(MOD_ID);
+		// Initializes our custom itemgroup "example:exampleGroup1"
+		RecipeBuilder.getItemGroup("example", "exampleGroup1");
+	}
+	@Override
 	public void onRecipesReady() {
 		// Basic shaped recipe that turns an 'H' pattern of dirt into a deactivated mobspawner
 		RecipeBuilder.Shaped(MOD_ID)
@@ -33,7 +36,7 @@ public class ExampleMod implements ModInitializer, RecipeEntrypoint {
 
 		// Slightly more complicated shaped recipe using the alternative initializer
 		RecipeBuilder.Shaped(MOD_ID, "WC", "CW")
-			.addInput('W', Block.wool, 2) // Green wool
+			.addInput('W', Block.wool, 13) // Green wool
 			.addInput('C', Block.cactus)
 			.create("basicShapedExample2", new ItemStack(Block.planksOakPainted, 20, 7)); // makes 20 grey planks
 
@@ -61,17 +64,17 @@ public class ExampleMod implements ModInitializer, RecipeEntrypoint {
 		xShapeTemplate.addInput('X', Item.basket).create("templateExample4", Block.seat.getDefaultStack());
 		xShapeTemplate.addInput('X', Block.dirt).create("templateExample5", Block.limestoneCarved.getDefaultStack());
 
+		// Register a new item group consistent of the items specified
+		RecipeBuilder.addItemsToGroup("example", "exampleGroup1", Item.diamond, Block.seat, Block.spongeWet, Item.flag);
+
+		// Modify an existing recipe group, this adds the coal block to the coal ores group
+		RecipeBuilder.addItemsToGroup("minecraft","coal_ores", Block.blockCoal);
+
 		// Shapeless Recipe examples
 		RecipeBuilder.Shapeless(MOD_ID)
 			.addInput(Item.toolAxeDiamond)
 			.addInput("minecraft:wools") // Sets the recipe to except any stack inside the minecrafts:wools item group
 			.create("itemGroupExample", new ItemStack(Item.diamond, 128));
-
-		// Register a new item group consistent of the items specified
-		Registries.ITEM_GROUPS.register("example:exampleGroup1", Registries.stackListOf(Item.diamond, Block.seat, Block.spongeWet, Item.flag));
-
-		// Modify an existing recipe group, this adds the coal block to the coal ores group
-		Registries.ITEM_GROUPS.getItem("minecraft:coal_ores").add(Block.blockCoal.getDefaultStack());
 
 		// Shapeless recipe using multiple recipe groups
 		RecipeBuilder.Shapeless(MOD_ID)
