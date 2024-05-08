fd061cc9df402b613892a29e56230f7d6be73195
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2024-05-08 03:41:17 -0500

    setup

--- src/main/java/turniplabs/examplemod/ExampleMod.java
+++ src/main/java/turniplabs/examplemod/ExampleMod.java
@@ -3,12 +3,10 @@ package turniplabs.examplemod;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import turniplabs.halplibe.helper.BlockBuilder;
 import turniplabs.halplibe.util.GameStartEntrypoint;
-import turniplabs.halplibe.util.RecipeEntrypoint;
 
 
-public class ExampleMod implements ModInitializer, GameStartEntrypoint, RecipeEntrypoint {
+public class ExampleMod implements ModInitializer, GameStartEntrypoint {
     public static final String MOD_ID = "examplemod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     @Override
@@ -26,8 +24,4 @@ public class ExampleMod implements ModInitializer, GameStartEntrypoint, RecipeEn
 
 	}
 
-	@Override
-	public void onRecipesReady() {
-
-	}
 }
